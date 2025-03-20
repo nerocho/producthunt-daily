57a93ce94d5aeeb4961a14a6db8b6cbeba55b671
NeroCho <nero_cho@163.com>
2025-03-20 14:30:14 +0800

    chore: 初始化项目配置并添加每日热门生成工作流
    添加了.gitignore文件以忽略node_modules目录，初始化了package.json和pnpm-lock.yaml文件以管理项目依赖。同时，创建了GitHub Actions工作流文件generate_daily_hot.yml，用于每日自动生成ProductHunt热门内容。还添加了index.js文件，用于从ProductHunt API获取数据并生成JSON文件。

---
+++ index.js
@@ -0,0 +1,65 @@
+import dayjs from 'dayjs';
+import fs from 'node:fs';
+
+const developer_token = process.env.PRODUCTHUNT_DEVELOPER_TOKEN;
+
+let hotDate = dayjs().subtract(1, 'days').format('YYYY-MM-DD');
+let posts = await GetProductHuntDailyHot(hotDate);
+
+fs.writeFileSync(`hot/${hotDate}.json`, JSON.stringify(posts));
+
+async function GetProductHuntDailyHot(d) {
+  let url = 'https://api.producthunt.com/v2/api/graphql';
+
+  let headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${developer_token}`,
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0',
+  };
+
+  let query = `
+  {
+      posts(order: VOTES, postedAfter: "${d}T00:00:00Z", postedBefore: "${d}T23:59:59Z", after: "${cursor}") {
+        nodes {
+          id
+          name
+          tagline
+          description
+          votesCount
+          createdAt
+          featuredAt
+          website
+          url
+          media {
+            url
+            type
+            videoUrl
+          }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }`;
+
+  let cursor = '';
+  let hasNextPage = true;
+  let posts = [];
+  while (hasNextPage && posts.length < 30) {
+    let response = await fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(query),
+    });
+    let data = await response.json();
+    posts = posts.concat(data.data.posts);
+
+    hasNextPage = data.pageInfo.hasNextPage;
+    cursor = data.pageInfo.cursor;
+  }
+
+  return posts;
+}
